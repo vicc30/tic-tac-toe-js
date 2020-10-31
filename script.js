45faa49f6cb6566b5853d2ead94d5e71f3d415a1
Victor Cruz <vicc23@gmail.com>
2020-10-31 03:29:31 -0600

    Added play move script

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const gameInit = (() => {
     const boardSize = 3;
+    const selection = 'X';
     const createBoard = () => {
         const gameBoardContainer = document.getElementById('board');
-        for (let i = 1; i <= boardSize*boardSize; i++) {
+        for (let i = 1; i <= boardSize * boardSize; i++) {
             const square = document.createElement('div');
             square.id = `square-${i}`;
             square.className = 'square';
+            square.setAttribute("onclick", `gameInit.playMove(this.id)`);
             gameBoardContainer.append(square);
         }
-    }
+    };
+
+    const playMove = (square) => {
+        if (!document.getElementById(`${square}-moved`)) {
+            const ubication = document.getElementById(square);
+            const play = document.createElement('p');
+            play.id = `${square}-moved`;
+            ubication.append(play);
+            const text = document.createTextNode(selection);
+            play.appendChild(text);
+        } else {
+            alert('Select other move');
+        }
+    };
     return {
-        createBoard
+        createBoard,
+        playMove
     }
 })();
 
