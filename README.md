d7f4c3e32255870a09ae1ac95fa798b13dc3ed75
Victor Cruz <vicc23@gmail.com>
2020-11-02 12:49:17 -0600

    Alternate and track moves

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ This excercise is part of Odin Project JavaScript Path.
 ## Odin Project Excercise
 
 1. Set up your project with a HTML, CSS and Javascript files and get the Git repo all set up.
+2. You’re going to store the gameboard as an array inside of a Gameboard object, so start there! Your players are also going to be stored in objects… and you’re probably going to want an object to control the flow of the game itself.
--- script.js
+++ script.js
@@ -1,32 +1,56 @@
 const gameInit = (() => {
     const boardSize = 3;
-    const selection = 'X';
     const createBoard = () => {
         const gameBoardContainer = document.getElementById('board');
         for (let i = 1; i <= boardSize * boardSize; i++) {
             const square = document.createElement('div');
-            square.id = `square-${i}`;
+            square.id = `${i}`;
             square.className = 'square';
-            square.setAttribute("onclick", `gameInit.playMove(this.id)`);
+            square.setAttribute("onclick", `gameBoard.playMove(this.id)`);
             gameBoardContainer.append(square);
         }
     };
+    return {
+        createBoard
+    }
+})();
 
+const gameBoard = (() => {
+    let selection = '';
+    const selected = (select) => {
+        selection = select;
+    };
+    const xMoves = [];
+    const oMoves = [];
     const playMove = (square) => {
+        let nextMove = '';
+        //If no selection starts with X
+        selection === '' ? nextMove = 'X' : nextMove = selection;
+
         if (!document.getElementById(`${square}-moved`)) {
             const ubication = document.getElementById(square);
             const play = document.createElement('p');
             play.id = `${square}-moved`;
             ubication.append(play);
-            const text = document.createTextNode(selection);
+            const text = document.createTextNode(nextMove);
             play.appendChild(text);
+
+            if(nextMove==='X'){
+                xMoves.push(square);
+                selection='O';
+            } else {
+                oMoves.push(square);
+                selection='X';
+            }
+            console.log('O moves:' + oMoves);
+            console.log('X moves:'+xMoves);
         } else {
             alert('Select other move');
         }
     };
     return {
-        createBoard,
-        playMove
+        playMove,
+        selected
     }
 })();
 
